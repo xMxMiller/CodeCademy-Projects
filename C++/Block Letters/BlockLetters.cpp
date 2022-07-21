0129b22d4be7e51de4ee2889207a7ad2f7dabcba
liammiller@librem.one <02694440+xMxMiller@users.noreply.github.com>
2022-07-21 19:49:56 +0100

    Created Main Branch with ReadMe and 2 projects

---
+++ C++/Block Letters/BlockLetters.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() 
+{
+  std::cout << "L    \tM   M\n";
+  std::cout << "L    \tMM MM\n";
+  std::cout << "L    \tMM MM\n";
+  std::cout << "L    \tM M M\n";
+  std::cout << "L    \tM   M\n";
+  std::cout << "L    \tM   M\n";
+  std::cout << "LLLLL\tM   M\n";
+}
---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+My solutions to codecademy projects within their sylybus.
