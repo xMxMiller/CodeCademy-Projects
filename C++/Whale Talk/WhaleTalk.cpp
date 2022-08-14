da4eb9b4b3b90b33dc11d8c0761c10b30cc7b3bb
xMxMiller <xMxMiller@user.noreply.github.com>
2022-08-14 23:07:28 +0100

    Added Whale Talk Project

---
+++ C++/Whale Talk/WhaleTalk.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+
+  std::string input = "hi, humanu";
+
+  std::vector<char> vowels = {'a','e','i','o','u'};
+
+  std::vector<char> result;
+
+    for (int i = 0;i < input.length(); i++){
+      for (int c = 0; c < vowels.size(); c++){
+        if (input[i] == vowels[c]){
+          if (input[i] == 'e' || input[i] == 'u'){
+            result.push_back(input[i]);
+            result.push_back(input[i]);
+          }
+          else{
+            result.push_back(input[i]);
+          }
+        }
+      }
+    }
+
+    for (int b = 0; b < result.size(); b++){
+      std::cout << result[b];
+    }
+    std::cout << "\n";
+}
