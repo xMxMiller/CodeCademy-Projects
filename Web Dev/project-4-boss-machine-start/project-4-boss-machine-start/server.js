582bf030b9e109513a7c9ff2a1efa58ff16dbc4c
Liam Miller <92694440+xMxMiller@users.noreply.github.com>
2024-04-05 15:58:02 +0100

    Added cors to project

--- Web Dev/project-4-boss-machine-start/project-4-boss-machine-start/server.js
+++ Web Dev/project-4-boss-machine-start/project-4-boss-machine-start/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
 module.exports = app;
@@ -9,7 +10,7 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
 
