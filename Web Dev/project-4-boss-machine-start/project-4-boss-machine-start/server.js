af7373e2dcab242f05cf9344edc935bfb2d148c5
Liam Miller <92694440+xMxMiller@users.noreply.github.com>
2024-04-05 16:00:23 +0100

    Added body-parser middleware.

--- Web Dev/project-4-boss-machine-start/project-4-boss-machine-start/server.js
+++ Web Dev/project-4-boss-machine-start/project-4-boss-machine-start/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 module.exports = app;
@@ -13,7 +14,7 @@ const PORT = process.env.PORT || 4001;
 app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
